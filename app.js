39826ffd61d4cfc31d824bdad5c570bb55e80c1c
ashraful <ashrafulmomin2@gmail.com>
2023-12-16 12:05:31 +0530

    added a

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 let h1 = document.querySelector("h1");
 h1.style.backgroundColor="red";
+//form added
+let form = document.querySelector("form");
---
+++ features.js
@@ -0,0 +1,4 @@
+let btn = document.createElement("button");
+btn.style.color='red';
+// button added
+let  a =25;
